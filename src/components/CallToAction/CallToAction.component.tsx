faee788450c2b6fdb8f1c3e8d0e92016145db2de
Tommaso Ribaudo <tommasoribaudo1@gmail.com>
2023-02-27 22:07:11 -0600

    call to action component background implementation

--- src/components/CallToAction/CallToAction.component.tsx
+++ src/components/CallToAction/CallToAction.component.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import banner from '../../assets/images/backgrounds/bg-1.jpg';
 
 const CallToAction = () => {
   return (
-    <section className="call-to-action section-sm bg-1 overly">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <h2>Check Availability and Prices</h2>
-            <p>discover our discounts.</p>
-            <a href="#contact-us" className="btn btn-main">Reserve Now</a>
+    
+        <section className="call-to-action section-sm bg-1 overly" style={{ backgroundImage: `url(${banner})`, height: 500, backgroundAttachment: 'fixed', backgroundPosition : 'center center' }}>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h2>Check Availability and Prices</h2>
+                <p>discover our discounts.</p>
+                <a href="#contact-us" className="btn btn-main">Reserve Now</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      
   );
 }
 

852fcfba5d13cf2281b20ce8a08a638be9e78405
David Alfonso <davalf99@gmail.com>
2023-03-19 10:52:21 -0600

    Images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import TucanoImage from '../images/about/tucano-sm.jpg'
+import GecoImage from '../images/about/geco-sm.jpg'
+import PappagalloImage from '../images/about/pappagallo-sm.jpg'
+import RanaImage from '../images/about/rana-sm.jpg'
+
+export {TucanoImage, GecoImage,PappagalloImage, RanaImage}
--- src/components/OurHomes/OurHomes.component.tsx
+++ src/components/OurHomes/OurHomes.component.tsx
@@ -1,101 +1,49 @@
 import { FC } from 'react';
 import HomeCard from './Components/HomeCard.component';
 import './OurHomes.style.scss'
+import { TucanoImage, GecoImage, PappagalloImage, RanaImage } from '../../assets/images'
 
 const OurHomes: FC = () => {
-  return (
-    <section id="house-list" className="bg-one about section">
-        <div className="container">
-            <div className="row">
 
-                {/* <!-- section title --> */}
-                <div className="title text-center wow fadeIn" data-wow-duration="1500ms">
-                    <h2>Our <span className="color">Homes</span> </h2>
-                    <div className="border"></div>
+    const homes = [
+        {
+            name: 'Tucano',
+            gusestNumber: 4,
+            parking: false,
+            image: TucanoImage
+        },
+        {
+            name: 'Geco',
+            gusestNumber: 5,
+            parking: true,
+            image: GecoImage
+        },
+        {
+            name: 'Pappagallo',
+            gusestNumber: 4,
+            parking: false,
+            image: PappagalloImage
+        },
+        {
+            name: 'Rana',
+            gusestNumber: 4,
+            parking: true,
+            image: RanaImage
+        },
+    ]
+    return (
+        <section id="house-list" className="bg-one about section">
+            <div className="container">
+                <div className="row">
+                    <div className="title text-center wow fadeIn" data-wow-duration="1500ms">
+                        <h2>Our <span className="color">Homes</span> </h2>
+                        <div className="border"></div>
+                    </div>
+                    {homes.map(({ name, gusestNumber, parking, image }) => <HomeCard guestNumber={gusestNumber} parking={parking} name={name} image={image} />)}
                 </div>
-                {/* <!-- /section title --> */}
-
-                {/* <!-- About item --> */}
-               <HomeCard />
-                {/* <!-- End About item --> */}
-
-                {/* <!-- About item --> */}
-                <div className="col-md-3 text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="250ms">
-                    <a data-scroll href="#contact-us">
-                        <div className="block ">
-                            <div className="icon-box center-block">
-                                <img src="images/about/geco-sm.jpg" alt="House geco" className="img-responsive img-rounded our-homes-img-sm our-homes-img-m" />
-                            </div>
-                            {/* <!-- Express About Yourself --> */}
-                            <div className="content text-center">
-                                <h3 className="highlight">Geco</h3>
-                                <div style={{display:"block"}}>
-                                    <div className="container-rounded-border border-highlight"><i className="fas fa-user highlight"></i> <b 
-                                    // style="font-size: larger;" 
-                                    className="highlight">X5</b></div>
-                                    <i className="fas fa-snowflake fa-lg highlight"></i>
-                                    <i className="fas fa-utensils fa-lg highlight"></i>
-                                    <i className="fas fa-wifi fa-lg highlight"></i>
-                                    <i className="fas fa-parking fa-lg highlight"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                {/* <!-- End About item --> */}
-
-                {/* <!-- About item --> */}
-                <div className="col-md-3 text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="500ms">
-                    <a data-scroll href="#contact-us">
-                        <div className="block ">
-                            <div className="icon-box center-block">
-                                <img src="images/about/pappagallo-sm.jpg" alt="House pappagallo" className="img-responsive img-rounded our-homes-img-sm our-homes-img-m" />
-                            </div>
-                            {/* <!-- Express About Yourself --> */}
-                            <div className="content text-center">
-                                <h3 className="highlight">Pappagallo</h3>
-                                <div style={{display:"block"}}>
-                                    <div className="container-rounded-border border-highlight"><i className="fas fa-user highlight"></i> <b 
-                                    // style="font-size: larger;" 
-                                    className="highlight">X4</b></div>
-                                    <i className="fas fa-snowflake fa-lg highlight"></i>
-                                    <i className="fas fa-utensils fa-lg highlight"></i>
-                                    <i className="fas fa-wifi fa-lg highlight"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                {/* <!-- End About item --> */}
-{/* 
-                <!-- About item --> */}
-                <div className="col-md-3 text-center wow fadeInUp" data-wow-duration="500ms" data-wow-delay="750ms">
-                    <a data-scroll href="#contact-us" >
-                        <div className="block ">
-                            <div className="icon-box center-block">
-                                <img src="images/about/rana-sm.jpg" alt="House rana" className="img-responsive img-rounded our-homes-img-sm our-homes-img-m" />
-                            </div>
-                            {/* <!-- Express About Yourself --> */}
-                            <div className="content text-center">
-                                <h3 className="highlight">Rana</h3>
-                                <div style={{display:"block"}}>
-                                    <div className="container-rounded-border border-highlight"><i className="fas fa-user highlight"></i> <b 
-                                    // style="font-size: larger;" 
-                                    className="highlight">X4</b></div>
-                                    <i className="fas fa-snowflake fa-lg highlight"></i>
-                                    <i className="fas fa-utensils fa-lg highlight"></i>
-                                    <i className="fas fa-wifi fa-lg highlight"></i>
-                                    <i className="fas fa-parking fa-lg highlight"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default OurHomes;

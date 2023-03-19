2bdf356583d3b0800f529be44a48503d67a471ea
Tommaso Ribaudo <tommasoribaudo1@gmail.com>
2023-03-19 15:48:34 -0600

    removed useless icon and changed color of a text

--- src/components/TipCard/TipCard.component.tsx
+++ src/components/TipCard/TipCard.component.tsx
@@ -2,7 +2,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ExecFileSyncOptionsWithBufferEncoding } from "child_process";
 import { IImageDescription } from "../../utils/constants"
-
+import './TipCard.style.scss'
 interface ITipCard {
   roomType: string;
   roomDescription: string;
@@ -12,10 +12,10 @@ interface ITipCard {
 export const TipCard = ({ roomType, roomDescription, folderName }: ITipCard) => {
   return (
     <div className="caption d-flex  align-items-center flex-column">
-      <div className="search-icon image-popup d-flex justify-content-center align-items-center" data-effect="mfp-with-zoom" data-lightbox="image-1">
+      {/* <div className="search-icon image-popup d-flex justify-content-center align-items-center" data-effect="mfp-with-zoom" data-lightbox="image-1">
         <FontAwesomeIcon icon={faSearch} color='white' fontSize={"20px"} />
-      </div>
-      <h4>Casa {folderName} - {roomType}</h4>
+      </div> */}
+      <h4 className="tipcard-caption">Casa {folderName} - {roomType}</h4>
       <p>{roomDescription}</p>
     </div>
   )

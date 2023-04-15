3f5b15f8918a69ae6c40ff65642fc18c011e6b4c
David Alfonso <davalf99@gmail.com>
2023-04-15 14:20:43 -0600

    Added param navigation for listings

--- src/pages/Listing/Listing.page.tsx
+++ src/pages/Listing/Listing.page.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import './Listing.style.scss'
 import OtherListings from "./components/OtherListings/OtherListings.component";
+import { useParams } from "react-router-dom";
 const Listing = () => {
+
+    const {listing} = useParams()
+
+    const listings = [
+        {name: 'Tucano'},
+        {name: 'Geco'},
+        {name: 'Pappagallo'},
+        {name: 'Rana'},
+    ]
     return (
     <div className="listingContainer">
-     {/* <Container className="listingContainer"> */}
         <Row className="subContainer">
          <Col className="otherOptions col" lg={2}><OtherListings/></Col>
          <Col className="info col" lg={7}>2</Col>
          <Col className="book col" lg={3}>3</Col>
         </Row>
-     {/* </Container> */}
     </div>
     )
 }

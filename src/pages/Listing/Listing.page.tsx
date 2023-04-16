2cfdc148cc6af333cfb7f072a43c338facfe0765
David Alfonso <davalf99@gmail.com>
2023-04-15 18:11:39 -0600

    Adding other listings

--- src/pages/Listing/Listing.page.tsx
+++ src/pages/Listing/Listing.page.tsx
@@ -3,20 +3,23 @@ import { Col, Row } from "react-bootstrap";
 import './Listing.style.scss'
 import OtherListings from "./components/OtherListings/OtherListings.component";
 import { useParams } from "react-router-dom";
-const Listing = () => {
+import { ListingType } from "../../utils/types";
+import { TucanoImage } from "../../assets/images";
 
+
+const Listing = () => {
     const {listing} = useParams()
 
-    const listings = [
-        {name: 'Tucano'},
-        {name: 'Geco'},
-        {name: 'Pappagallo'},
-        {name: 'Rana'},
+    const listings: ListingType[] = [
+        {name: 'Tucano', mainImage: TucanoImage },
+        {name: 'Geco', mainImage: TucanoImage},
+        {name: 'Pappagallo', mainImage: TucanoImage},
+        {name: 'Rana', mainImage: TucanoImage},
     ]
     return (
     <div className="listingContainer">
         <Row className="subContainer">
-         <Col className="otherOptions col" lg={2}><OtherListings/></Col>
+         <Col className="otherOptions col" lg={{order:'first', span:2}} xs={{order:'last', span:12}}><OtherListings listings={listings} currentListing={listing || ''} /></Col>
          <Col className="info col" lg={7}>2</Col>
          <Col className="book col" lg={3}>3</Col>
         </Row>

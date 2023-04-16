9c8372cbb2307528cdcc1bb7a61a727e60625f98
David Alfonso <davalf99@gmail.com>
2023-04-15 18:11:39 -0600

    Adding other listings

--- src/pages/Listing/components/OtherListings/OtherListings.component.tsx
+++ src/pages/Listing/components/OtherListings/OtherListings.component.tsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, {FC} from "react";
 import { Stack } from "react-bootstrap";
 import './OtherListings.style.scss'
+import { ListingType } from "../../../../utils/types";
+import { useNavigate } from "react-router-dom";
 
-const OtherListings = () => {
+interface IOtherListing {
+    currentListing: string
+    listings: ListingType[]
+}
 
-    const listings = [{name: 'test1'},{name: 'test2'}]
+const OtherListings: FC<IOtherListing> = ({currentListing, listings}) => {
+    const naviagate = useNavigate()
     return (
     <div className="cont d-flex justify-content-center">
-        <Stack gap={5} className=" d-flex align-items-center">
-            {listings.map(({name})=>{
-                return (
-                <div className="listing d-flex align-items-end">
+        <Stack gap={5} className=" d-flex align-items-center subCont">
+            {listings.map(({name, mainImage})=>{
+                return name !== currentListing ? ( //TODO do bootsrtap thing tomake change row/column
+                <div style={{backgroundImage: `url(${mainImage})`,}} className="listing d-flex align-items-end"  onClick={()=>{naviagate(`/listing/${name}`)}}>
                     <div className="name">{name}</div>
                 </div>
-                )
+                ) : null
             })}
         </Stack>
     </div>)

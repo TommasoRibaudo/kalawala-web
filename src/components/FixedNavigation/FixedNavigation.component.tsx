fb680e7f8811bb720a35544cbacf00510c83a425
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2023-05-31 02:44:50 +0200

    Highest id

--- src/components/FixedNavigation/FixedNavigation.component.tsx
+++ src/components/FixedNavigation/FixedNavigation.component.tsx
@@ -4,13 +4,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from "react-router-dom";
+import { getHighestId } from "../../assets/blogs/blogs";
 interface IFixedNavigation {
-  isBlog:boolean
+  isBlog: boolean
 }
-const FixedNavigation = ({isBlog}:IFixedNavigation) => {
+const FixedNavigation = ({ isBlog }: IFixedNavigation) => {
   const [isActive, setIsActive] = useState<boolean>(true);
   const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
-  
+
   const handleToggleClick = (event: any) => {
     if (event.currentTarget.classList.contains('collapsed')) {
       setIsCollapsed(true);
@@ -26,7 +27,7 @@ const FixedNavigation = ({isBlog}:IFixedNavigation) => {
     setIsActive(false);
   };
   const handleBlogClick = () => {
-    navigate(`blog/1`);
+    navigate(`blog/` + getHighestId());
     setTimeout(() => {
       window.scrollTo(0, 0);
     }, 0);
@@ -70,11 +71,11 @@ const FixedNavigation = ({isBlog}:IFixedNavigation) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleClick} />
         <Navbar.Collapse id="basic-navbar-nav" className={`${isCollapsed}`} >
           <Nav className="navMenu" >
-            <Nav.Link href="/#body" className={`navText ${(isActive && !isBlog) ? 'active':''}`} >Home</Nav.Link>
+            <Nav.Link href="/#body" className={`navText ${(isActive && !isBlog) ? 'active' : ''}`} >Home</Nav.Link>
             <Nav.Link href="/#callToAction" className="navText" onClick={() => { handleLinkClick("#callToAction") }}>Availability</Nav.Link>
             <Nav.Link href="/#portfolio" className="navText" onClick={() => { handleLinkClick("#portfolio") }}>Photos</Nav.Link>
             <Nav.Link href="/#contact-us" className="navText" onClick={() => { handleLinkClick("#contact-us") }}>Contact</Nav.Link>
-            <Nav.Link className={`navText ${(isActive && isBlog) ? 'active':''}`} onClick={() => { handleBlogClick() }}>Blog</Nav.Link>
+            <Nav.Link className={`navText ${(isActive && isBlog) ? 'active' : ''}`} onClick={() => { handleBlogClick() }}>Blog</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

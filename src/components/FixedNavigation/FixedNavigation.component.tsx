5dc92721d37ee9a3244d5dd62743c2d32811b8ea
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2024-08-21 19:24:35 -0600

    added colors, retouched menu

--- src/components/FixedNavigation/FixedNavigation.component.tsx
+++ src/components/FixedNavigation/FixedNavigation.component.tsx
@@ -5,6 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from "react-router-dom";
 import { getHighestId } from "../../assets/blogs/blogs";
+import SolidBars from "../../assets/images/bars-solid.svg";
 interface IFixedNavigation {
   isBlog: boolean
 }
@@ -28,26 +29,28 @@ const FixedNavigation = ({ isBlog }: IFixedNavigation) => {
   };
   const handleBlogClick = () => {
     navigate(`/blog/` + `twodaysinpuertoviejo`);
-      //getHighestId());
+    //getHighestId());
     setTimeout(() => {
       window.scrollTo(0, 0);
     }, 0);
   };
 
   return (
-    <Navbar className="navigation" expand="lg" sticky="top" variant="dark">
+    <Navbar className="navigation" expand="lg" sticky="top" variant="dark " data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="/#body" className="d-flex align-items-center">
           <img src="https://i.imgur.com/yo53PM4.png" alt="logo" className="logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleClick} />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="dark-nav" onClick={handleToggleClick} >
+          <img src={SolidBars} style={{height:"25px"}}/>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav" className={`${isCollapsed}`} >
           <Nav className="navMenu" >
             <Nav.Link href="/#body" className={`navText ${(isActive && !isBlog) ? 'active' : ''}`} >Home</Nav.Link>
             <Nav.Link href="/#callToAction" className="navText" onClick={() => { handleLinkClick("#callToAction") }}>Availability</Nav.Link>
             <Nav.Link href="/#portfolio" className="navText" onClick={() => { handleLinkClick("#portfolio") }}>Photos</Nav.Link>
             <Nav.Link href="/#contact-us" className="navText" onClick={() => { handleLinkClick("#contact-us") }}>Contact</Nav.Link>
-            <Nav.Link  href="/twodaysinpuertoviejo" className={`navText ${(isActive && isBlog) ? 'active' : ''}`} >Blog</Nav.Link>
+            <Nav.Link href="/twodaysinpuertoviejo" className={`navText ${(isActive && isBlog) ? 'active' : ''}`} >Blog</Nav.Link>
             {/* <Nav.Link className={`navText ${(isActive && isBlog) ? 'active' : ''}`} onClick={() => { handleBlogClick() }}>Blog</Nav.Link> Blog is currently unavailable*/}
           </Nav>
         </Navbar.Collapse>

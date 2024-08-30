8483662df37bdf39f32620582aa174b049f39a46
Just-Gazzu <neitonch18@gmail.com>
2024-08-30 14:05:14 -0600

    homeES

--- src/components/FixedNavigation/FixedNavigation.component.tsx
+++ src/components/FixedNavigation/FixedNavigation.component.tsx
@@ -6,9 +6,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from "react-router-dom";
 import { getHighestId } from "../../assets/blogs/blogs";
 import SolidBars from "../../assets/images/bars-solid.svg";
+import { ES } from 'country-flag-icons/react/3x2'
+
 interface IFixedNavigation {
   isBlog: boolean
 }
+
 const FixedNavigation = ({ isBlog }: IFixedNavigation) => {
   const [isActive, setIsActive] = useState<boolean>(true);
   const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
@@ -27,33 +30,30 @@ const FixedNavigation = ({ isBlog }: IFixedNavigation) => {
     navigate(`/${url}`);
     setIsActive(false);
   };
-  const handleBlogClick = () => {
-    navigate(`/blog/` + `twodaysinpuertoviejo`);
-    //getHighestId());
-    setTimeout(() => {
-      window.scrollTo(0, 0);
-    }, 0);
-  };
 
   return (
-    <Navbar className="navigation" expand="lg" sticky="top" variant="dark " data-bs-theme="dark">
+    <Navbar className="navigation" expand="lg" sticky="top" variant="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="/#body" className="d-flex align-items-center">
           <img src="https://drive.google.com/thumbnail?id=1z6ekQR8hrkzw_-6rUuNeRxyakDo2pdfn&sz=w1000" alt="logo" className="logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="dark-nav" onClick={handleToggleClick} >
-          <img src={SolidBars} style={{height:"25px"}}/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="dark-nav" onClick={handleToggleClick}>
+          <img src={SolidBars} style={{ height: "25px" }} />
         </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav" className={`${isCollapsed}`} >
-          <Nav className="navMenu" >
-            <Nav.Link href="/#body" className={`navText ${(isActive && !isBlog) ? 'active' : ''}`} >Home</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navMenu me-auto">
+            <Nav.Link href="/#body" className={`navText ${(isActive && !isBlog) ? 'active' : ''}`}>Home</Nav.Link>
             <Nav.Link href="/#callToAction" className="navText" onClick={() => { handleLinkClick("#callToAction") }}>Availability</Nav.Link>
             <Nav.Link href="/#portfolio" className="navText" onClick={() => { handleLinkClick("#portfolio") }}>Photos</Nav.Link>
             <Nav.Link href="/#contact-us" className="navText" onClick={() => { handleLinkClick("#contact-us") }}>Contact</Nav.Link>
-            <Nav.Link href="/twodaysinpuertoviejo" className={`navText ${(isActive && isBlog) ? 'active' : ''}`} >Blog</Nav.Link>
-            {/* <Nav.Link className={`navText ${(isActive && isBlog) ? 'active' : ''}`} onClick={() => { handleBlogClick() }}>Blog</Nav.Link> Blog is currently unavailable*/}
+            <Nav.Link href="/twodaysinpuertoviejo" className={`navText ${(isActive && isBlog) ? 'active' : ''}`}>Blog</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <div className="navbar-flag">
+          <a href="/HomeES">
+            <ES title="Español" />
+            </a>
+        </div>
       </Container>
     </Navbar>
   );
---
+++ src/pages/Home/Home.pageES.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import FixedNavigation from "../../components/FixedNavigation/FixedNavigation.component";
+import Discover from "../../components/Discover/Discover.component";
+import OurHomes from "../../components/OurHomes/OurHomes.component";
+import CallToAction from "../../components/CallToAction/CallToAction.component";
+import Testimonial from "../../components/Testimonial/Testimonial.component";
+import ContactUs from "../../components/ContactUs/ContactUs.component";
+import WelcomeSlider from "../../components/WelcomeSlider/WelcomeSlider.component";
+import Portfolio from "../../components/Portfolio/Portfolio.component";
+
+import { houseDataEngList } from '../../utils/constants';
+
+const HomeES = () => {
+
+  return (
+    <div id="body">
+      <WelcomeSlider />
+      <FixedNavigation isBlog={false}/>
+      <OurHomes houseDataList={houseDataEngList}/>
+      <Discover />
+      <CallToAction />
+      <Portfolio />
+      {/* <Testimonial /> */}
+      <ContactUs />
+    </div>
+  )
+}
+
+export default HomeES;

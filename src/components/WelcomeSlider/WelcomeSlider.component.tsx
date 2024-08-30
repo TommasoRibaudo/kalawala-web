accf77fed01e1bb0cdd90fa72ba047dd8404f88a
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2024-08-30 12:03:34 -0600

    pictures and minor changes to style

--- src/components/WelcomeSlider/WelcomeSlider.component.tsx
+++ src/components/WelcomeSlider/WelcomeSlider.component.tsx
@@ -3,7 +3,7 @@ import './WelcomeSlider.style.scss';
 import Button from 'react-bootstrap/Button';
 
 const WelcomeSlider = () => {
-  let banner = 'https://drive.google.com/thumbnail?id=1uahGi9p2U4rcoVK2hq55HMgsrtXI0xWy&sz=w1000';
+  let banner = 'https://drive.google.com/thumbnail?id=1c25flui45r6oVIvPMqKLemVsWgjbhsAH&sz=w1000';
   return (
     <section className="hero-area overlay" style={{ backgroundImage: `url(${banner})`}}>
       <div className = "block" >

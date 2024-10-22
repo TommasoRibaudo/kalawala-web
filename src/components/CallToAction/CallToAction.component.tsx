03ea722ff826877ee69e2dd9cebb22fbbd882900
Just-Gazzu <neitonch18@gmail.com>
2024-10-22 17:59:03 -0600

    Home_Smoobo

--- src/components/CallToAction/CallToAction.component.tsx
+++ src/components/CallToAction/CallToAction.component.tsx
@@ -6,16 +6,29 @@ import Smoobu2 from '../Smoobu2/Smoobu2.component';
 
 const CallToAction = () => {
   return (
-    <section id="callToAction" className="call-to-action overly bg-1" style={{ backgroundImage: `url(https://drive.google.com/thumbnail?id=16VEmEiyt0Szm76Zth7MTuOvvHZ689W2f&sz=w1000)` }}>
+    <section id="callToAction" className="call-to-action overly bg-1" style={{ backgroundImage: `url(https://drive.google.com/thumbnail?id=1NoeXMkl7483dB0hVfCYuGPbqKTkRpQXq&sz=w1000)` }}>
       <div className="test section-sm">
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center" >
-              <p style={{fontWeight: 400, fontSize: 28}}>Check Availability and Prices</p>
+              <p style={{fontWeight: 700, fontSize: 28}}>Check Availability and Prices</p>
               <p>discover our discounts.</p>
+              <div 
+            className='Smoobo' 
+            style={{ 
+              backgroundColor: 'rgba(245, 242, 233, 0.75)', 
+              borderRadius: '15px', 
+              padding: '20px', 
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', 
+            }}
+            >
               <Smoobu2 />
             </div>
+            <p style={{fontWeight: 700, fontSize: 28}}>Got any questions?</p>
+            <p style={{fontWeight: 400, fontSize: 20}}>Contact us +506 84632276</p>
           </div>
+            </div>
+            
         </div>
       </div>
 

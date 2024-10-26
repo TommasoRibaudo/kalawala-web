e5301807e0fd50a50d4a55be7870d1a832f97cb4
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2024-10-25 18:06:40 -0600

    skeleton of new homes

---
+++ src/pages/Listing/staticPages/ListingVillaCoral.page.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
+import '../Listing.style.scss'
+import OtherListings from "../components/OtherListings/OtherListings.component";
+import Smoobu from "../../../components/Smoobu/Smoobu.component";
+import ImagesContainer from "../components/ImagesContainer/ImagesContainer.component";
+import ImagesModal from "../components/ImagesModal/ImagesModal.component";
+import { useParams } from "react-router-dom";
+import { homesSnippet } from "../../../utils/constants";
+import { HouseDataType, ListingType } from "../../../utils/types";
+import Amenities from "../components/Amenities/Amenities.component";
+import { AmenityType } from "../../../utils/types";
+import { houseDataList } from "../../../utils/constants";
+import FixedNavigation from "../../../components/FixedNavigation/FixedNavigation.component";
+import { Helmet } from "react-helmet";
+import { useMediaQuery } from '@react-hook/media-query';
+
+
+const ListingVillaCoral = () => {
+    const listing = 'Geco'
+    const isScreenSmall = useMediaQuery('(max-width: 992px)');
+    const amenities: AmenityType[] = [
+        { icon: 'ac', name: '2 A/C Units' },
+        { icon: 'ac', name: '2 A/C Units' },
+        { icon: 'ac', name: '2 A/C Units' }
+    ]
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const houseData: HouseDataType | undefined = houseDataList.find((house) => house.name === listing);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+    }, [])
+    //const description = houseData?.description.split('<br/>');
+    //const neighborhood = houseData?.neighborhood.split('<br/>');
+    return (
+        <div className={`listingContainer ${show && 'modal-open'}`}>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>House Geco - Pet Friendly Home in Puerto Viejo</title>
+                <meta name="description" content="Located in the heart of town, this house has space for up to 5 people and features a fully equipped kitchen, a bathroom, 2 A/C units, and a private parking lot." />
+                <link rel="canonical" href="https://www.reservaskalawala.com/Geco" />
+            </Helmet>
+            <FixedNavigation isBlog={false} />
+            <Row className="subContainer">
+                <Col className="otherOptions col" lg={windowWidth <= 1199 ? { order: 'last', span: 2 } : { order: 'first', span: 2 }} md={{ order: 'last', span: 12 }} order={windowWidth <= 1199 ? { lg: 'last' } : { lg: 'first' }} sm={{ order: 'last', span: 12 }} xs={{ order: 'last', span: 12 }}>
+                    <OtherListings listings={homesSnippet} currentListing={listing || ''} />
+                </Col>
+                <Col className="info col" lg={{ order: 'first', span: 7 }} md={windowWidth <= 991 ? { order: 'first', span: 12 } : { order: 'first', span: 8 }} sm={12} xs={12}>
+                    <div className="heading">
+                        <h1 className="title">House Geco</h1>
+                        <h3 className="location">Puerto Viejo de Talamanca, Limón, Costa Rica</h3>
+                        {isScreenSmall && (
+                            <div className="button-hold"><Button className='btn-darker' href="#smoobuComp">Book Online Now!</Button></div>)}
+                    </div>
+                    <ImagesContainer showModal={handleShow} houseName={listing!} />
+                    <div className="amenaties">
+                        <Amenities amenities={houseData?.amenities as AmenityType[]} />
+                    </div>
+                    <div className="description">
+                        <p>
+                            Located in the heart of town, this house has space for up to 5 people and features a fully equipped kitchen, a bathroom, 2 A/C units, and a private parking lot. Our prime location offers easy access to both the town center and the most beautiful beaches that Puerto Viejo has to offer.
+                            <br />
+                        </p>
+                        <p>
+                            Most shops and restaurants are just a short walk away, and there is a nearby jungle path that runs along the ocean and leads to natural pools in the coral and to Cocles.
+                            <br />
+                        </p>
+                        <p>
+                            All of the spaces described here are private, including the fully equipped kitchen and bathroom. You'll have everything you need to make yourself at home.
+                            <br />
+                        </p>
+                        <p>
+                            We offer cleaning services for reservations of 5 nights or longer. Our team will contact you during your stay to coordinate a convenient time for the cleaning.
+                            <br />
+                        </p>
+                        <p>
+                            Do you have a special request? We would be more than happy to accommodate you if we can. Please don't hesitate to let us know.
+                            <br />
+                        </p>
+                        <p>
+                            If you require a pack- and - play crib during your stay, please inform us ahead of time. We'll make sure to set it up in your room during our cleaning process.
+                            <br />
+                        </p>
+                        <p>
+                            Puerto Viejo is a popular destination for tourists from all over the world, thanks to its stunning surroundings. The town boasts immense beaches that are surrounded by tropical rainforest, as well as two National Parks (Manzanillo and Cahuita). At night, the town comes alive with a lively and active nightlife scene. When you stay here, you'll be able to fully immerse yourself in everything that makes Puerto Viejo unique.
+                            <br />
+                        </p>
+                        <p>
+                            The house is located close to beach access that eventually leads to Cocles. Along the way, you'll have the opportunity to spot a variety of animals and admire natural pools in the coral. There's even a hidden sightseeing spot waiting to be discovered!
+                            <br />
+                        </p>
+                        <p>
+                            Getting around in Puerto Viejo and its surroundings is easiest by renting a bike or a scooter. However, there is also a reliable public bus service available that can take you to Cahuita, Manzanillo, and Sixaola. If you prefer to drive, we can accommodate cars as well. We offer private parking but please let us know if you have a larger pickup truck that requires additional space.
+                            <br />
+                        </p>
+                    </div>
+
+                </Col>
+                <Col id="smoobuComp" className="book col" lg={3} md={windowWidth <= 991 ? { span: 12 } : { order: 'first', span: 4 }} sm={{ span: 12 }} xs={{ span: 12 }}>
+                    <Smoobu homeCode={houseData!.houseCode} />
+                </Col>
+            </Row>
+            {show && <ImagesModal closeModal={handleClose} houseName={listing!} />}
+        </div>
+    )
+
+}
+
+export default ListingVillaCoral;

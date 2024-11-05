b20f8f2a1c181f1fa3659585c80bc084d6ba80ab
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2024-11-05 15:01:52 -0600

    discovercomponent for rib + removed some unused imports

--- src/pages/Home/Home.pageES.rib.tsx
+++ src/pages/Home/Home.pageES.rib.tsx
@@ -1,28 +1,21 @@
 import React from "react";
-import FixedNavigation from "../../components/FixedNavigation/FixedNavigation.component";
-import Discover from "../../components/Discover/Discover.component";
-import OurHomes from "../../components/OurHomes/OurHomes.component";
-import CallToAction from "../../components/CallToAction/CallToAction.component";
-import Testimonial from "../../components/Testimonial/Testimonial.component";
 import ContactUs from "../../components/ContactUs/ContactUs.component";
-import WelcomeSlider from "../../components/WelcomeSlider/WelcomeSlider.component";
 import Portfolio from "../../components/Portfolio/Portfolio.component";
-
-import {ribHouseDataEngList, VillasDataList } from '../../utils/constants';
+import {VillasDataList } from '../../utils/constants';
 import CallToActionES from "../../components/CallToAction/CallToAction.componentES";
 import FixedNavigationRibES from "../../components/FixedNavigation/FixedNavigation.componentRIBES";
 import WelcomeSliderES from "../../components/WelcomeSlider/WelcomeSlider.componentES";
 import OurHomesES from "../../components/OurHomes/OurHomes.componentES";
+import DiscoverRIBES from "../../components/Discover/Discover.componentRIBES";
 
 const HomeRibES = () => {
 
   return (
     <div id="body">
-      RIBHOLDING PLACE HOLDER ES
       <WelcomeSliderES />
       <FixedNavigationRibES isBlog={false}/>
       <OurHomesES houseDataList={VillasDataList}/>
-      <Discover />
+      <DiscoverRIBES />
       <CallToActionES />
       <Portfolio />
       <ContactUs />

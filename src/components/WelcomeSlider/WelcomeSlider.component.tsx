5c7f9f7d94865d0dcb3b9f4e71328d886218c56c
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2025-09-07 23:45:17 -0600

    smoobu comp

--- src/components/WelcomeSlider/WelcomeSlider.component.tsx
+++ src/components/WelcomeSlider/WelcomeSlider.component.tsx
@@ -6,26 +6,26 @@ import Smoobu2 from '../Smoobu2/Smoobu2.component';
 const WelcomeSlider = () => {
   let banner = 'https://drive.google.com/thumbnail?id=1c25flui45r6oVIvPMqKLemVsWgjbhsAH&sz=w1000';
   return (
-    <section className="hero-area overlay" style={{ backgroundImage: `url(${banner})`}}>
-      <div className = "block" >
+    <section className="hero-area overlay" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="block" >
         <h1>RESERVAS KALAWALA</h1>
         <p id="short-description">Fully equipped vacation homes in the heart of Puerto Viejo and Playa Chiquita.</p>
         <br />
 
-              <div 
-            className='Smoobo' 
-            style={{ 
-              backgroundColor: 'rgba(245, 242, 233, 0.86)', 
-              borderRadius: '15px', 
-              padding: '5px', 
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', 
-            }}
-            >
-              <Smoobu2 targetId="welcomeSliderSmoobu" />
-            </div>
-          <br />
+        <div
+          className='Smoobo'
+          style={{
+            backgroundColor: 'rgba(245, 242, 233, 0.86)',
+            borderRadius: '15px',
+            padding: '5px',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <Smoobu2 targetId="welcomeSliderSmoobu" />
+        </div>
+        <br />
       </div >
-    </section > 
+    </section >
   );
 };
 

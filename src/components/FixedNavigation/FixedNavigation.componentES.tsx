7334a58c22f31c352c7d4d4aadd3e538c1950e30
TommasoRibaudo <49354857+TommasoRibaudo@users.noreply.github.com>
2025-09-14 15:25:34 -0600

    otherlistings in es listing now redirect to spanish pages

--- src/components/FixedNavigation/FixedNavigation.componentES.tsx
+++ src/components/FixedNavigation/FixedNavigation.componentES.tsx
@@ -58,9 +58,14 @@ const FixedNavigationES = ({ isBlog }: IFixedNavigationES) => {
             loading="eager"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="dark-nav" onClick={handleToggleClick}>
-          <img src={SolidBars} style={{ height: "25px" }} />
-        </Navbar.Toggle>
+        <div className="mobile-controls">
+          <div className="mobile-flag">
+            <LanguageSwitcher />
+          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="dark-nav" onClick={handleToggleClick}>
+            <img src={SolidBars} style={{ height: "25px" }} />
+          </Navbar.Toggle>
+        </div>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="navMenu me-auto">
             <Nav.Link href="HomeES#body" className={`navText ${(isActive && !isBlog) ? 'active' : ''}`} onClick={closeMenu}>Inicio</Nav.Link>
